a71c266bf33803e09b2ef0d76a188149d61f7df7
Kaan <kaanyrdkl@gmail.com>
2020-06-12 18:40:22 +0300

    Entities & ShopContext

---
+++ src/ApplicationCore/Entities/Brand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class Brand
+    {
+        public string BrandName { get; set; }
+    }
+}
---
+++ src/Infrastructure/Data/ShopContext.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationCore.Entities;
+using Infrastructure.Data.Config;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // https://www.learnentityframeworkcore.com/configuration/fluent-api
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
